5da5eaae4ce08d48cbe09de0cf0a57f2382102d0
guangzhi.sun <guangzhi.sun@bytedance.com>
2026-03-21 01:19:47 +0800

    modify paths

--- train.sh
+++ train.sh
@@ -4,12 +4,15 @@ pip install qwen-omni-utils[decord] -U
 pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu126
 pip install rouge-score
 
+expdir=exp/qwen25_omni_sft_caption
+mkdir -p $expdir
+
 torchrun --nproc_per_node=2 --master_port=12345 train.py \
   --model_name_or_path Qwen/Qwen2.5-Omni-7B \
-  --dataset data/train_data.json \
-  --val_dataset data/valid_data.json \
-  --output_dir ./qwen25_omni_sft_out \
-  --per_device_train_batch_size 3 \
+  --dataset data/train_data_caption.json \
+  --val_dataset data/valid_data_caption.json \
+  --output_dir $expdir \
+  --per_device_train_batch_size 1 \
   --gradient_accumulation_steps 1 \
   --learning_rate 5e-5 \
   --num_train_epochs 5 \
